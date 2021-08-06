1318b2a88840faddf19ba67a7e6de19171bd284c
tugrulpinar <tugrulpinar96@gmail.com>
2021-08-06 11:36:22 -0400

    removed sqlite

--- leave_app/settings.py
+++ leave_app/settings.py
@@ -97,12 +97,12 @@ CACHES = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # SQLite runs locally
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # PostgreSQL runs locally
 # DATABASES = {
